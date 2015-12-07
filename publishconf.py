42a2b252298fe8a205aa65206f3b52df374b57be
Magnun Leno <magnun.leno@gmail.com>
2015-12-07 15:21:42 -0200

    Corrige URL de publicação

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://calangohackerclub.github.io'
+SITEURL = 'http://magnunleno.github.io/calangohc'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
